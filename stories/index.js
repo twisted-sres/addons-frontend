c1c7f086f1952d06e32932a4cc289aac4b2e8809
William Durand <will+git@drnd.me>
2018-11-24 19:52:58 +0100

    wip

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,13 @@
+import sinon from 'sinon';
+
 // Add global styles
 import 'amo/components/App/styles.scss';
 import 'core/css/inc/lib.scss';
 
 import './setup/styles.scss';
 
-import './ui/Badge';
-import './ui/Button';
+global.sinon = sinon.createSandbox();
+
+// Automagically require all stories.
+const req = require.context('./ui', false, /\.js$/);
+req.keys().forEach(req);
